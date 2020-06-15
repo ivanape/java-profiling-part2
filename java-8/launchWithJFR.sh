4d223c078e0a05d0091c21dd4a07e35ba43cf884
Iván Álvarez <ialvarezpereira@gmail.com>
2020-06-15 09:16:04 +0200

    Added JFR recording

---
+++ java-8/launchWithJFR.sh
@@ -0,0 +1,3 @@
+java \
+  -XX:+UnlockCommercialFeatures -XX:+FlightRecorder -XX:+UnlockDiagnosticVMOptions -XX:+DebugNonSafepoints -XX:FlightRecorderOptions=stackdepth=1024 \
+  -cp target/classes com.github.ivanape.main.PrimeMain 80000
--- java-8/src/main/java/com/github/ivanape/main/PrimeMain.java
+++ java-8/src/main/java/com/github/ivanape/main/PrimeMain.java
@@ -1,13 +1,17 @@
 package com.github.ivanape.main;
 
+import com.github.ivanape.common.DateHelper;
 import com.github.ivanape.examples.PrimeNumbers;
 
 public class PrimeMain {
 
-    public static void main(String[] args) {
-        PrimeNumbers primeNumbers = new PrimeNumbers();
+    public static void main(String[] args) throws InterruptedException {
+        Thread.sleep(20000);
         Integer max = Integer.parseInt(args[0]);
+        System.out.println(String.format("%s - starting the work with value: %d", DateHelper.now(), max));
+        PrimeNumbers primeNumbers = new PrimeNumbers();
         primeNumbers.generateNumbers(max);
+        System.out.println(String.format("%s - All is done!", DateHelper.now()));
     }
 
 }
