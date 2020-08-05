d47a2a2678b7f59dd00ae4581c8728d01beb7f34
Iván Álvarez <ialvarezpereira@gmail.com>
2020-08-05 09:18:10 +0200

    Update examples

--- deadLock/src/main/java/com/github/programwar/main/RunThreadDeadLock.java
+++ deadLock/src/main/java/com/github/programwar/main/RunThreadDeadLock.java
@@ -1,2 +1,69 @@
+package com.github.programwar.main;
+
 public class RunThreadDeadLock {
+
+    private static class LeftRightDeadLock implements Runnable {
+        private static final Object left = new Object();
+        private static final Object right = new Object();
+        private int number = 0;  // change value of number to 4000,  to find thread starvation issue.
+
+        public void doSomething(int number) {
+            for (int i = 0; i < number; i++) {
+                char[] tmp = new char[1024 * 1024];
+                tmp[1] = 'a';
+                System.out.printf("doSomething %s%n", Thread.currentThread().getName());
+            }
+        }
+
+        public void leftRight() {
+            synchronized (left) {
+                synchronized (right) {
+                    System.out.printf("%s -> leftRight calling doSomething%n", Thread.currentThread().getName());
+                    doSomething(number);
+                }
+            }
+        }
+
+        public void rightLeft() {
+            synchronized (right) {
+                synchronized (left) {
+                    System.out.printf("%s -> rightLeft calling doSomething%n", Thread.currentThread().getName());
+                    doSomething(number);
+                }
+            }
+        }
+
+        public void run() {
+            System.out.printf("%s -> run%n", Thread.currentThread().getName());
+
+            try {
+                while (true) {
+                    leftRight();
+                    rightLeft();
+                }
+            } catch (Exception e) {
+                System.out.println(e.getMessage());
+                System.out.flush();
+            }
+        }
+
+    }
+
+    public static void main(String[] args) {
+        LeftRightDeadLock first = new LeftRightDeadLock();
+        LeftRightDeadLock second = new LeftRightDeadLock();
+        LeftRightDeadLock third = new LeftRightDeadLock();
+
+        Thread tFirst = new Thread(first);
+        tFirst.setName("tFirst");
+        Thread tSecond = new Thread(second);
+        tSecond.setName("tSecond");
+        Thread tThird = new Thread(third);
+        tThird.setName("tThird");
+
+        tFirst.start();
+        tSecond.start();
+        //tThird.start();
+    }
+
 }
--- primeNumbers/launchWithJFR.sh
+++ primeNumbers/launchWithJFR.sh
@@ -1,3 +1,5 @@
+#! /bin/bash
+
 java \
   -XX:+UnlockCommercialFeatures -XX:+FlightRecorder -XX:+UnlockDiagnosticVMOptions -XX:+DebugNonSafepoints -XX:FlightRecorderOptions=stackdepth=1024 \
   -cp target/classes com.github.ivanape.main.PrimeMain 80000
