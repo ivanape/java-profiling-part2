0f290fb2b19891c40b87fc2c3da5f517b718c6d5
Iván Álvarez <ialvarezpereira@gmail.com>
2020-08-14 09:09:21 +0200

    Update exampels

--- memoryLeak/memoryLeak.sh
+++ memoryLeak/memoryLeak.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 cd target/classes
 java -XX:+FlightRecorder -XX:+UnlockDiagnosticVMOptions -XX:+DebugNonSafepoints -XX:FlightRecorderOptions=stackdepth=1024\
-    -XX:StartFlightRecording=settings=profile,dumponexit=true,filename="$(pwd)/memoryleak.jfr"\
+    -XX:StartFlightRecording=settings=profile,dumponexit=true,filename="$(pwd)/memoryleak.jfr",path-to-gc-roots=true\
     -Xms2G -Xmx2G org.programwar.examples.MemoryLeak
