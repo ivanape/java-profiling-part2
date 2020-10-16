ca064bcac5ef6a4aa077c67e0d0aded3d2ac9c3d
Iván Álvarez <ialvarezpereira@gmail.com>
2020-10-16 19:09:43 +0200

    Update example

--- JFR_03_latencies/latencies/src/main/java/com/programwar/Logger.java
+++ JFR_03_latencies/latencies/src/main/java/com/programwar/Logger.java
@@ -1,25 +1,18 @@
 package com.programwar;
 
 public class Logger {
-	private static Logger myInstance = new Logger();
-	
-	public static Logger getLogger() {
-		return myInstance;
-	}
+    private static Logger myInstance = new Logger();
 
+    public static Logger getLogger() {
+        return myInstance;
+    }
 
-	public synchronized void log(String text) {
-		LogEvent event = new LogEvent(CustomEventLatencies.LOG_EVENT_TOKEN, text);
-		event.begin();
-		// Do logging here
-		// Write the text to a database or similar...
-		try {
-			// Simulate that this takes a little while.
-			Thread.sleep(200);
-		} catch (InterruptedException e) {
-			// Don't care.
-		}
-		event.end();
-		event.commit();
-	}
+    public synchronized void log(String text) {
+        try {
+            Thread.sleep(20);
+            System.out.println(text);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
 }
