b4ea5bb66efab39ebd5282ab15f23ba2c253d359
Iván Álvarez <ialvarezpereira@gmail.com>
2020-11-16 09:02:05 +0100

    Update latencies example

--- JFR_03_latencies/latencies/src/main/java/com/programwar/Logger.java
+++ JFR_03_latencies/latencies/src/main/java/com/programwar/Logger.java
@@ -7,7 +7,7 @@ public class Logger {
         return myInstance;
     }
 
-    public synchronized void log(String text) {
+    public void log(String text) {
         try {
             Thread.sleep(20);
             System.out.println(text);
